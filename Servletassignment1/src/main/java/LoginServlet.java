9776908133f117e9fe67eb001b1e77a9897c6329
Chandankumar07HR <87484659+Chandankumar07HR@users.noreply.github.com>
2022-11-22 17:07:20 +0530

    Add files via upload

---
+++ Servletassignment1/src/main/java/LoginServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+public class LoginServlet extends HttpServlet {  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+                    throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+        request.getRequestDispatcher("link.html").include(request, response);  
+          
+        String name=request.getParameter("name");  
+        String password=request.getParameter("password");  
+          
+        if(password.equals("chandan07")){  
+        out.print("Welcome, "+name);  
+        HttpSession session=request.getSession();  
+        session.setAttribute("name",name);  
+        }  
+        else{  
+            out.print("Sorry the username or password you entered is wrong!");  
+            request.getRequestDispatcher("login.html").include(request, response);  
+        }  
+        out.close();  
+    }  
+}  
